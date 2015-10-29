087e3d30a9159a27bd9a9d178e52c170128003ed
Petar Petrov <mindfreeze@abv.bg>
2015-10-29 18:04:19 +0200

    initial code commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# OpenSeadragonRGB
+
+An OpenSeadragon plugin to allow reading the rgb values of image pixels.
+
+## Usage
+
+Include `dist/openseadragonrgb.js` after OpenSeadragon in your html. Then after you create a viewer:
+
+    var rgbPlugin = viewer.rgb(options);
+
+### Options
+
+    viewer.selection({
+        onMouseMove: function(color) {},
+    });
+
+### Get values
+
+You can get the RGBA value at an arbitrary point using viewport coordinates like this:
+
+    var color = rgbPlugin.getValueAt(x, y);
+
+Then `color` (wheter from `getValueAt()` or passed to the onMouseMove callback) will contain:
+
+    {
+        r: Number, // value for Red channel
+        g: Number, // value for Green channel
+        b: Number, // value for Blue channel
+        a: Number, // value for Alpha channel
+        image: OpenSeadragon.TiledImage // image that contains this pixel if there is any
+    }
---
+++ src/rgb.js
@@ -0,0 +1,100 @@
+(function($) {
+    'use strict';
+
+    if (!$.version || $.version.major < 2) {
+        throw new Error('This version of OpenSeadragonRGB requires OpenSeadragon version 2.0.0+');
+    }
+
+    $.Viewer.prototype.rgb = function(options) {
+        if (!this.rgbInstance || options) {
+            options = options || {};
+            options.viewer = this;
+            this.rgbInstance = new $.RGB(options);
+        }
+        return this.rgbInstance;
+    };
+
+
+    /**
+    * @class RGB
+    * @classdesc Allows access to RGB values of pixels
+    * @memberof OpenSeadragon
+    * @param {Object} options
+    */
+    $.RGB = function ( options ) {
+
+        $.extend( true, this, {
+            // internal state properties
+            viewer:                  null,
+
+            // options
+            onImageHover:             null,
+        }, options );
+
+        if (this.onImageHover) {
+            this.tracker = new $.MouseTracker({
+                element:            this.viewer.canvas,
+                moveHandler:        $.delegate( this, onMouseMove ),
+            });
+        }
+    };
+
+    $.extend( $.RGB.prototype, $.ControlDock.prototype, /** @lends OpenSeadragon.RGB.prototype */{
+        /**
+         * Get RGB values of cancas coordinates
+         * @method
+         * @param {OpenSeadragon.Point} canvasX the point in canvas coordinate system.
+         * @param {Number} canvasX X coordinate in canvas coordinate system.
+         * @param {Number} canvasY Y coordinate in canvas coordinate system.
+         * @return {Object|false} An object containing r,g,b properties or false if this is not supported.
+         */
+        getValueAt: function( canvasX, canvasY ) {
+            var point = arguments.length === 1 ? canvasX : new $.Point(canvasX, canvasY);
+            var viewer = this.viewer;
+            var result = viewer.drawer.getRgbAt(point);
+            if (result) {
+                // console.log(viewer.drawer.canvas.width);
+                // console.log(viewer.drawer.canvas.offsetWidth);
+                var image;
+                var imagePoint;
+                var size;
+                for (var i = 0; i < viewer.world.getItemCount(); i++) {
+                    image = viewer.world.getItemAt(i);
+                    size = image.getContentSize();
+                    imagePoint = image.viewerElementToImageCoordinates(point);
+                    if (imagePoint.x >= 0 && imagePoint.y >= 0 && imagePoint.x <= size.x && imagePoint.y <= size.y) {
+                        // point is inside an image
+                        result.image = image;
+                    }
+                }
+            }
+            return result;
+        },
+    });
+
+    function onMouseMove(event) {
+        this.onImageHover(this.getValueAt(event.position));
+    }
+
+    /**
+     * Get RGB values of cancas coordinates
+     * @method
+     * @param {OpenSeadragon.Point} point the point in image coordinate system.
+     * @return {Object|false} An object containing r,g,b properties or false if this is not supported.
+     */
+    $.Drawer.prototype.getRgbAt = function(point) {
+        if (!this.useCanvas) {
+            return false;
+        }
+        var ratio = $.pixelDensityRatio;
+        var color = this._getContext()
+                        .getImageData(point.x * ratio, point.y * ratio, 1, 1).data; // rgba e [0,255]
+        return {
+            r: color[0],
+            g: color[1],
+            b: color[2],
+            a: color[3]
+        };
+    };
+
+})(OpenSeadragon);
